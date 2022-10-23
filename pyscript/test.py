541f10ca34f02f37012f59a6d06f13076eb720bd
Hitkliff <Hitkliff.dev@gmail.com>
2022-10-23 11:29:07 +0300

    lesson 1

---
+++ pyscript/test.py
@@ -0,0 +1,18 @@
+#.join()
+valuesText = ['10' , '20' , '30']
+res = ','.join(valuesText)
+print(res)
+
+#upper(), lower(), capitalize(), swapcase()
+string1 = 'Hello, World'
+stringRes = string1.upper()
+print(stringRes)
+
+stringRes = string1.lower()
+print(stringRes)
+
+stringRes = string1.swapcase()
+print(stringRes)
+
+stringRes = string1.capitalize()
+print(stringRes)
